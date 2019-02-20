60ecee4f5c127fb6035def32c758841b4f46454b
Jared Harbison <jared.harbison@gmail.com>
2019-02-20 22:41:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,8 @@ class Dog
     sql = <<-SQL
     SELECT * FROM dogs WHERE name = ?
     SQL
-    DB[:conn].execute()
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
   end 
   
 end 
