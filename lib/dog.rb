13a3fce91752aa4aad120bd1643a3671641483b6
Jared Harbison <jared.harbison@gmail.com>
2019-02-20 22:08:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   attr_reader :id 
   attr_accessor :name, :breed 
   
-  def initialize(id = nil, name: name, breed: breed)
+  def initialize(id=nil, name: name, breed: breed)
     @id = id 
     @name = name 
     @breed = breed 
