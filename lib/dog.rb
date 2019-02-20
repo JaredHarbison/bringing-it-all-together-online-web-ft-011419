7e33cc8847f26fb6d37e8c9ab0c749a8f1c15a35
Jared Harbison <jared.harbison@gmail.com>
2019-02-20 22:46:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -83,7 +83,7 @@ class Dog
   end 
   
   def update 
-    sql <<-SQL
+    sql = <<-SQL
     UPDATE dogs SET name = ?, breed = ?, WHERE id = ?
     SQL
     DB[:conn].execute(sql, self.name, self.breed, self.id)
